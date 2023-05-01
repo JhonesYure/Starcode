1d939d18d7777d656f3c6d773764cc1b02eed525
Jhones Yure <103959700+JhonesYure@users.noreply.github.com>
2023-05-01 14:22:32 -0300

    teste de server v2.0

--- server/conect.php
+++ server/conect.php
@@ -1,14 +1,4 @@
 <?php
-$servername = "149.100.155.1";
-$username = "u820043173_contato";
-$password = "Yure1995+";
-$dbname = "u820043173_contato";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-    die("Conexão falhou: " . mysqli_connect_error());
-}
-echo "Conexão bem sucedida!";
 
 ?>
