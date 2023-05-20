bc4a926903c461dd0a8ed9b032a2b5c0a2e0957a
Jhones Yure <103959700+JhonesYure@users.noreply.github.com>
2023-05-20 16:09:51 -0300

    versção 2.0 do blog

--- services/index.js
+++ services/index.js
@@ -51,3 +51,34 @@ form.addEventListener('submit', function(event) {
   // Envia a solicitação AJAX com os dados do formulário em formato JSON
   xhr.send(jsonData);
 });
+
+
+//BLOG
+function mostrarItens() {
+  var divItensOcultos = document.getElementById('itens-ocultos');
+  var btnVerMais = document.getElementById('ver-mais-btn');
+
+  if (divItensOcultos.style.display === 'none') {
+    divItensOcultos.style.display = 'block';
+    btnVerMais.textContent = 'Ver Menos';
+  } else {
+    divItensOcultos.style.display = 'none';
+    btnVerMais.textContent = 'Ver Mais';
+  }
+}
+//
+
+
+window.addEventListener('scroll', function() {
+  var setaTopo = document.getElementById("voltar-topo");
+  var alturaPagina = document.documentElement.scrollHeight;
+  var alturaJanela = window.innerHeight;
+  var posicaoAtual = window.scrollY || window.pageYOffset;
+
+  if (posicaoAtual > (alturaPagina / 2) && posicaoAtual > alturaJanela) {
+    setaTopo.classList.add("show");
+  } else {
+    setaTopo.classList.remove("show");
+  }
+});
+
